fe0ae1c29528f44e0845bff5f9b268fa6ad4d89b
allibrdoine <allibrodine81@gmail.com>
2022-06-26 16:26:21 -0500

    html structure/display current time

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+//display current day/time
+var rightNow = document.querySelector('#currentDay');
+var now = moment().format('MMMM Do YYYY hh:mm a');
+    rightNow.textContent = now;
+
+
+//style text area so passed time is in gray, current is red, future is green
+
+
+
+//set up local storage to store changes in timeblock events
+
+
+
+//handle save button
