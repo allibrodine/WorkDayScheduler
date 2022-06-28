dfbd7fb3e9ee4fc12d8bc75431f8e17e428a1fa0
allibrdoine <allibrodine81@gmail.com>
2022-06-28 17:16:22 -0500

    save btn/localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,28 @@ var now = moment().format('MMMM Do YYYY hh:mm a');
 
 
 //style text area so passed time is in gray, current is red, future is green
+$(".hour").each(function() {
+    //var timeCheck = 
+
+    if (now == timeCheck) {
+        $(this).addClass("present");
+    } else if (now < timeCheck) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else if (now > timeCheck) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+});
 
 
 
-//set up local storage to store changes in timeblock events
+//handle save button, add content to localStorage
+$(".saveBtn").click(function(event) {
+    event.preventDefault();
+    var time = $(this).siblings(".hour").text();
+    var value = $(this).siblings(".description").val();
 
-
-
-//handle save button
+    localStorage.setItem(time, value);
+    
+});
