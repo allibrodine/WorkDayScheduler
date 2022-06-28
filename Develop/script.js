b18d680839130c5b43f805020777821488174d3e
allibrdoine <allibrodine81@gmail.com>
2022-06-28 18:23:21 -0500

    time tense color

--- Develop/script.js
+++ Develop/script.js
@@ -5,20 +5,22 @@ var now = moment().format('MMMM Do YYYY hh:mm a');
 
 
 //style text area so passed time is in gray, current is red, future is green
-$(".hour").each(function() {
-    //var timeCheck = 
+var timeColor = function() {
+    $(".time-block").each(function() {
+        var timeCheck = $(this).attr("id"); 
+            console.log(this);
 
-    if (now == timeCheck) {
-        $(this).addClass("present");
-    } else if (now < timeCheck) {
-        $(this).removeClass("present");
-        $(this).addClass("future");
-    } else if (now > timeCheck) {
-        $(this).removeClass("future");
-        $(this).addClass("past");
-    }
-});
-
+        if (now == timeCheck) {
+            $(this).addClass("present");
+        } else if (now < timeCheck) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else if (now > timeCheck) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
+};
 
 
 //handle save button, add content to localStorage
@@ -27,6 +29,7 @@ $(".saveBtn").click(function(event) {
     var time = $(this).siblings(".hour").text();
     var value = $(this).siblings(".description").val();
 
-    localStorage.setItem(time, value);
-    
+    localStorage.setItem(time, value); 
 });
+
+timeColor();
